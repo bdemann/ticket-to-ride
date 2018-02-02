bbd1d7471175fdb6cca590431304a2b8c5cf623a
Ben Nelson <benjamin.n.nelson@gmail.com>
2018-02-02 16:49:18 -0700

    Ben Demann did this.

---
+++ app/src/main/java/com/a340team/tickettoride/client/ClientRoot.java
@@ -0,0 +1,36 @@
+package com.a340team.tickettoride.client;
+
+import java.util.Observable;
+
+/**
+ * Created by BenNelson on 2/2/18.
+ *
+ */
+public class ClientRoot extends Observable {
+
+    private Player clientPlayer;
+    private Game clientGame;
+
+    public ClientRoot(Player player, Game game) {
+        this.clientPlayer = player;
+        this.clientGame = game;
+    }
+    public void setClientPlayer(Player player) {
+        this.clientPlayer = player;
+        setChanged();
+        notifyObservers();
+    }
+    public Player getClientPlayer(){
+        return clientPlayer;
+    }
+
+    public Game getClientGame() {
+        return clientGame;
+    }
+
+    public void setClientGame(Game clientGame) {
+        this.clientGame = clientGame;
+        setChanged();
+        notifyObservers();
+    }
+}
