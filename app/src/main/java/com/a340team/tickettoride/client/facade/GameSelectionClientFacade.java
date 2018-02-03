60e36da98235ad3c44f67c20c37bf5c3413d4161
Ben Nelson <benjamin.n.nelson@gmail.com>
2018-02-03 15:44:15 -0700

    Put in Client Facade Classes

---
+++ app/src/main/java/com/a340team/tickettoride/client/facade/GameSelectionClientFacade.java
@@ -0,0 +1,20 @@
+package com.a340team.tickettoride.client.facade;
+
+import com.a340team.tickettoride.shared.client.IGameSelectionClient;
+
+/**
+ *
+ * Created by BenNelson on 2/3/18.
+ */
+
+public class GameSelectionClientFacade implements IGameSelectionClient {
+    @Override
+    public void createGame() {
+
+    }
+
+    @Override
+    public void joinGame() {
+
+    }
+}
