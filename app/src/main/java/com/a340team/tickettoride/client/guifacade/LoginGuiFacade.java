7a9fc58b24cd8d96ceed220108420f9658413760
Mike Poret <mikecporet@gmail.com>
2018-02-04 22:48:58 -0700

    Mike got it to work!

--- app/src/main/java/com/a340team/tickettoride/client/guifacade/LoginGuiFacade.java
+++ app/src/main/java/com/a340team/tickettoride/client/guifacade/LoginGuiFacade.java
@@ -1,5 +1,7 @@
 package com.a340team.tickettoride.client.guifacade;
 
+import com.a340team.tickettoride.client.proxy.LoginClientProxy;
+
 /**
  *
  * Created by BenNelson on 2/3/18.
@@ -7,7 +9,17 @@ package com.a340team.tickettoride.client.guifacade;
 
 public class LoginGuiFacade {
 
-    public static void signIn() {
+    public static boolean signIn(String userName, String password) {
+        LoginClientProxy loginClientProxy = LoginClientProxy.SINGLETON;
+        loginClientProxy.signIn(userName, password);
+        return true;
+    }
 
+    public static boolean register(String userName, String password, String password_confirm){
+        if (!password.equals(password_confirm))
+            return false;
+        LoginClientProxy loginClientProxy = LoginClientProxy.SINGLETON;
+        loginClientProxy.register(userName, password);
+        return true;
     }
 }
