61218224618a59b528aa6601f976c330fb91f06d
Ben Nelson <benjamin.n.nelson@gmail.com>
2018-02-06 20:35:54 -0700

    Added some interfaces

---
+++ app/src/main/java/presenter/ILoginPresenter.java
@@ -0,0 +1,14 @@
+package presenter;
+
+/**
+ *
+ * Created by BenNelson on 2/6/18.
+ */
+
+public interface ILoginPresenter {
+
+    void registerPasswordChanged();
+    void confirmPasswordChanged();
+    void signIn();
+    void register();
+}
