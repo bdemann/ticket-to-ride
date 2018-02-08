a30a23985a436f090709f392c69dd7f5c1dfa9c8
Ben Nelson <benjamin.n.nelson@gmail.com>
2018-02-07 19:57:26 -0700

    edited the login view and presenter

--- app/src/main/java/presenter/LoginPresenter.java
+++ app/src/main/java/presenter/LoginPresenter.java
@@ -10,51 +10,26 @@ import view.LoginActivity;
 public class LoginPresenter implements ILoginPresenter {
 
 
-
-    private static LoginPresenter _instance;
-
-    public static LoginPresenter instance() {
-
-        if (_instance == null)
-            _instance = new LoginPresenter();
-
-        return _instance;
-    }
-
-    private LoginPresenter() { }
-
-    //Makes the singleton code a little cleaner
-    public static void signIn(String username, String password){
-        LoginPresenter.instance()._signIn(username,password);
-    }
-
-
-
-
     @Override
-    public void _registerPasswordChanged() {
+    public void registerPasswordChanged() {
 
     }
 
     @Override
-    public void _confirmPasswordChanged() {
+    public void confirmPasswordChanged() {
 
     }
 
     @Override
-    public void _signIn(String username, String password) {
+    public void signIn(String username, String password) {
         //Since this is called, we need to get the sign in information from the view
         //then we can communicate with the LoginGuiFacade
         System.out.println("USERNAME: " + username + "\n");
         System.out.println("PASSWORD: " + password + "\n");
-
-
-
-
     }
 
     @Override
-    public void _register() {
+    public void register() {
 
     }
 }
--- app/src/main/java/view/ILoginView.java
+++ app/src/main/java/view/ILoginView.java
@@ -5,12 +5,10 @@ package view;
  * Created by BenNelson on 2/6/18.
  */
 
-public interface ILoginView {
+interface ILoginView {
 
     void enableSignIn(boolean enabled);
     void enableRegister(boolean enabled);
-    void setUsername(String username);
-    void setPassword(String password);
     String getUsernameSignIn();
     String getPasswordSignIn();
     String getUsernameRegister();
