fcdb5e5c0a521f6ddba5f53def07ccb2a972a154
mporet <mikecporet@gmail.com>
2018-02-10 12:16:54 -0700

    Added Game Lobby and Game Activities

---
+++ app/src/main/java/view/GameAcitvity.java
@@ -0,0 +1,16 @@
+package view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.a340team.tickettoride.R;
+
+public class GameAcitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_acitvity);
+
+    }
+}
