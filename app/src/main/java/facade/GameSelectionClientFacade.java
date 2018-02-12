b201aac2efb75a2e6a3a7b08dd7d02be5ddf7dde
caussepa <caussepa@gmail.com>
2018-02-11 20:32:38 -0700

    check for number of players

--- app/src/main/java/facade/GameSelectionClientFacade.java
+++ app/src/main/java/facade/GameSelectionClientFacade.java
@@ -1,20 +1,30 @@
 package facade;
 
+import model.ClientRoot;
 import shared.client.IGameSelectionClient;
+import shared.model.Game;
+import shared.model.Player;
 
 /**
  *
  * Created by BenNelson on 2/3/18.
  */
 
-public class GameSelectionClientFacade implements IGameSelectionClient {
-    @Override
-    public void createGame() {
+public class GameSelectionClientFacade{
+    private static ClientRoot _clientRoot = ClientRoot.instance();
+
+    public void createGame(Game game) {
+        _clientRoot.setListGames(game);
 
     }
 
-    @Override
-    public void joinGame() {
+    public void joinedGame(Player player, Game game) {
+        _clientRoot.getListGames().get(game.getId()).addPlayer(player);
 
+        if(_clientRoot.getClientGame() != null){
+            if(game.getId() == _clientRoot.getClientGame().getId()){
+                _clientRoot.getClientGame().addPlayer(player);
+            }
+        }
     }
 }
