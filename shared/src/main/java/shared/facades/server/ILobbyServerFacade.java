aa2d85cceba56f60426d5dc3418da865d2ec87ec
Ben DeMann <benjamin.demann@gmail.com>
2018-03-05 16:28:27 -0700

    added a start game result

--- shared/src/main/java/shared/facades/server/ILobbyServerFacade.java
+++ shared/src/main/java/shared/facades/server/ILobbyServerFacade.java
@@ -2,12 +2,13 @@ package shared.facades.server;
 
 import shared.results.Result;
 import shared.model.interfaces.IGame;
+import shared.results.StartGameResult;
 
 /**
  * Created by Ben on 2/6/2018.
  */
 
 public interface ILobbyServerFacade {
-    Result startGame(IGame game, String username);
+    StartGameResult startGame(IGame game, String username);
     Result leaveGame(String username);
 }
---
+++ shared/src/main/java/shared/results/StartGameResult.java
@@ -0,0 +1,31 @@
+package shared.results;
+
+import java.util.List;
+
+import shared.command.ICommand;
+import shared.model.interfaces.IGameInfo;
+import shared.model.interfaces.IPlayer;
+
+/**
+ * Created by bdemann on 3/5/18.
+ */
+
+public class StartGameResult extends Result {
+
+    private IPlayer player;
+    private IGameInfo gameInfo;
+
+    public StartGameResult(IPlayer player, IGameInfo gameInfo, boolean success, List<ICommand> clientCommands, String userMessage) {
+        super(success, clientCommands, userMessage);
+        this.player = player;
+        this.gameInfo = gameInfo;
+    }
+
+    public IPlayer getPlayer() {
+        return player;
+    }
+
+    public IGameInfo getGameInfo() {
+        return gameInfo;
+    }
+}
