e5c82b2dd6b029e58957f3affe680568d741bedb
caussepa <caussepa@gmail.com>
2018-03-07 22:44:19 -0700

    get starter gameinfo

--- app/src/main/java/presenter/IGameInfoPresenter.java
+++ app/src/main/java/presenter/IGameInfoPresenter.java
@@ -1,8 +1,14 @@
 package presenter;
 
+import java.util.List;
+
+import shared.model.DestCard;
+import shared.model.interfaces.IGameInfo;
+
 /**
  * Created by paulinecausse on 3/7/18.
  */
 
 public interface IGameInfoPresenter {
+    IGameInfo getStarterGameInfo();
 }
