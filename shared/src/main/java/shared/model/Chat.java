45bc34e85d68797d5d980849cb44072797d82396
Ben DeMann <benjamin.demann@gmail.com>
2018-03-08 17:20:55 -0700

    made chat and game event implement the same interface in hopes of using only one recycler view for both of them

--- shared/src/main/java/shared/model/Chat.java
+++ shared/src/main/java/shared/model/Chat.java
@@ -3,17 +3,33 @@ package shared.model;
 import java.io.Serializable;
 
 import shared.model.interfaces.IPlayer;
+import shared.model.interfaces.Message;
 
 /**
  * Created by Ben on 2/6/2018.
  */
 
-public class Chat implements Serializable {
+public class Chat implements Serializable, Message {
     private Player speaker;
     private String message;
     private long time;
 
     public IPlayer getSpeaker() {
         return speaker;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public long getTime() {
+        return time;
+    }
+
+    @Override
+    public String getUsername() {
+        return speaker.getUsername();
     }
 }
---
+++ shared/src/main/java/shared/model/interfaces/Message.java
@@ -0,0 +1,12 @@
+package shared.model.interfaces;
+
+/**
+ * Created by bdemann on 3/8/18.
+ */
+
+public interface Message {
+
+    public String getMessage();
+    public long getTime();
+    public String getUsername();
+}
