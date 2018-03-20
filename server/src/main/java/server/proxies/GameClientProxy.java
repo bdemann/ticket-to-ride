cab7e0ab87ce42433ad6b291ea7e20485bbb0fc0
Benjamin DeMann <benjamin.demann@gmail.com>
2018-03-19 22:37:22 -0600

    hook up game client facade to the proxy

--- server/src/main/java/server/proxies/GameClientProxy.java
+++ server/src/main/java/server/proxies/GameClientProxy.java
@@ -4,6 +4,7 @@ import server.poller.ClientCommands;
 import shared.command.Command;
 import shared.command.ICommand;
 import shared.facades.client.IGameClientFacade;
+import shared.logging.Logger;
 import shared.model.interfaces.IGameInfo;
 
 /**
@@ -14,11 +15,12 @@ public class GameClientProxy implements IGameClientFacade {
     @Override
     public void updateGameInfo(IGameInfo gameInfo) {
         for(String username : gameInfo.getPlayers()) {
+            Logger.log("We are telling " + username + " about an update to the game.");
             ClientCommands.addCommand(username, _createUpdateCommand(gameInfo));
         }
     }
 
-    private static final String CLASS = "facade.LobbyClientFacade";
+    private static final String CLASS = "facade.GameClientFacade";
 
     private ICommand _createUpdateCommand(IGameInfo gameInfo) {
         Class<?>[] parmTypes = {IGameInfo.class};
