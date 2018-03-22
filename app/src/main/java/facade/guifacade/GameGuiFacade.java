c76c6b9b10255b2e2ce36c969188c11b84e2f3ad
caussepa <caussepa@gmail.com>
2018-03-22 12:54:46 -0600

    Fixed dest cards

--- app/src/main/java/facade/guifacade/GameGuiFacade.java
+++ app/src/main/java/facade/guifacade/GameGuiFacade.java
@@ -8,9 +8,12 @@ import proxies.GameServerProxy;
 import shared.command.Command;
 import shared.model.DestCardSet;
 import shared.model.DestCard;
+import shared.model.DestDeck;
 import shared.model.TrainCard;
 import shared.model.TrainCardSet;
+import shared.model.interfaces.IGame;
 import shared.model.interfaces.IGameInfo;
+import shared.model.interfaces.IPlayer;
 import shared.model.interfaces.IRoute;
 import shared.results.ClaimRouteResult;
 import shared.results.DrawCardsResult;
@@ -23,7 +26,7 @@ import shared.results.DrawTrainCardsResult;
 
 public class GameGuiFacade {
 
-    //TODO: change this method to get the destination cards, not the unresolved ones
+
     public static List<DestCard> getStarterDestinationCards(){
         DestCardSet s = ClientRoot.getClientPlayer().getUnresolvedDestCards();
         return s.toList();
@@ -50,6 +53,7 @@ public class GameGuiFacade {
     private static List<DestCard> _processDrawDestinationResults(DrawCardsResult cardResults) {
         if(cardResults != null){
             //TODO it was not my intention to have a cast like this... is there a way to change this? Maybe its not too big of a deal?
+            ClientRoot.getClientPlayer().setUnresolvedDestCards((List<DestCard>) cardResults.getCards());
             return (List<DestCard>) cardResults.getCards();
         }
         else {
@@ -59,7 +63,7 @@ public class GameGuiFacade {
 
     public static void discardDestinationCards(List<DestCard> chosenDestCards, DestCardSet cardsToRemove){
         GameServerProxy gsp = new GameServerProxy();
-        ClientRoot.getClientPlayer().setDestCards(chosenDestCards);
+        ClientRoot.getClientPlayer().addDestCards(chosenDestCards) ;
         DestCardSet destCardSet = new DestCardSet(chosenDestCards);
 
         gsp.discardDestCards(ClientRoot.getClientPlayer().getUsername(), destCardSet, cardsToRemove);
@@ -68,7 +72,13 @@ public class GameGuiFacade {
     public static List<Boolean> getCompleteDestination(){
         boolean isCompleted = false;
         List<Boolean> completedDestination = new ArrayList<>();
-        for(DestCard destCard : ClientRoot.getClientPlayer().getUnresolvedDestCards()){
+        for(DestCard destCard : ClientRoot.getClientPlayer().getDestCards()){
+            if(destCard.isCompleted()){
+                isCompleted = true;
+            }
+            else if(!destCard.isCompleted()){
+                isCompleted = false;
+            }
             completedDestination.add(isCompleted);
         }
 
