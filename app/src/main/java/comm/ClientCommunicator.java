75b028b5ae31a75c6de18bd60f02f59ac295e8df
Ben Nelson <benjamin.n.nelson@gmail.com>
2018-03-21 20:28:14 -0600

    Design by Contract stuff

--- app/src/main/java/comm/ClientCommunicator.java
+++ app/src/main/java/comm/ClientCommunicator.java
@@ -19,7 +19,7 @@ import shared.results.Result;
 
 public class ClientCommunicator {
 
-    private static final String SERVER_HOST = "192.168.0.13";
+    private static final String SERVER_HOST = "10.24.22.109";
     private static final String SERVER_PORT = "8080";
     private static final String URL_PREFIX = "http://" + SERVER_HOST + ":" + SERVER_PORT + "/";
     private static final String HTTP_POST = "POST";
