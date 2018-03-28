6f6083cb16553ed4ac8fc8ded3d24f6cb6140e50
bennelsn <benjamin.n.nelson@gmail.com>
2018-03-28 12:05:20 -0600

    Fixed card matching comment

--- server/src/main/java/server/facades/GameServerFacade.java
+++ server/src/main/java/server/facades/GameServerFacade.java
@@ -85,7 +85,7 @@ public class GameServerFacade implements IGameServerFacade {
             //Check that the cards are the same color as the route.
             boolean cardsMatch = _colorsMatch(cards, route);
             if (!cardsMatch) {
-                return new ClaimRouteResult(false, player.getTrainCardHand(),game.getGameInfo(), ClientCommands.getCommandList(username), "Cards did not match\nthe color of the route.");
+                return new ClaimRouteResult(false, player.getTrainCardHand(),game.getGameInfo(), ClientCommands.getCommandList(username), "Cards did not match.");
             }
             boolean sufficientTrains = _playerHasEnoughTrains(route.getLength(), player);
             if(!sufficientTrains){
